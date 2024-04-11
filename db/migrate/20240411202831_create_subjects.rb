b5598e891bc77db9d5d4840fd44d1ab5dc3ebe38
MatveySviadysh <mkomp06@gmail.com>
2024-04-11 23:32:42 +0300

    write validates

--- db/migrate/20240411202831_create_subjects.rb
+++ db/migrate/20240411202831_create_subjects.rb
@@ -1,10 +1,10 @@
 class CreateSubjects < ActiveRecord::Migration[7.1]
   def change
     create_table :subjects do |t|
-      t.string :name
-      t.text :description
-      t.integer :semester_id
-      t.string :assessment_type
+      t.string :name, presence: true, length: { minimum: 3 }
+      t.text :description, presence: true
+      t.integer :semester_id, presence: true
+      t.string :assessment_type, presence: true
 
       t.timestamps
     end
