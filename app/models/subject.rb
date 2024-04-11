6502d1957db25d6a20790c47a88bdce0f1ef7846
MatveySviadysh <mkomp06@gmail.com>
2024-04-12 00:04:36 +0300

    wrote validates for the model

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,11 @@
 class Subject < ApplicationRecord
+
+  validates :name, presence: true, length: { minimum: 3 }
+
+  validates :description, presence: true
+
+  validates :semester_id, presence: true
+
+  validates :assessment_type, presence: true
+
 end
