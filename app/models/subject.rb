70922dac48f3157a0c963a52c7a5234e5b9aaace
MatveySviadysh <mkomp06@gmail.com>
2024-04-12 00:05:12 +0300

    wrote associations for the model

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -8,4 +8,12 @@ class Subject < ApplicationRecord
 
   validates :assessment_type, presence: true
 
+
+  # belongs_to :semester
+  # has_many :intermediate_attestations
+  # has_many :record_books
+  # has_many :teachers_subjects
+  # has_many :retakes
+  # has_many :attendance
+
 end
