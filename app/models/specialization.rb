be17b49b97b11cc16000f1e4c2f8a98611551f39
stanislav <stas.plisko.99@mail.ru>
2024-04-13 21:27:49 +0300

    validates and assotiation for specialization

--- app/models/specialization.rb
+++ app/models/specialization.rb
@@ -1,2 +1,6 @@
 class Specialization < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates_format_of :name, with: /\A[a-zA-Zа-яА-Я\s]+\z/, message: 'only allows letters and spaces'
+
+    has_many :students, dependent: :destroy
 end
