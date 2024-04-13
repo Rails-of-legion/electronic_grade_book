313ef1ebabd1896f27e2fc3f22f80dac9c2a226f
Sergick12 <58103310+Sergick12@users.noreply.github.com>
2024-04-13 17:06:52 +0300

    added validate and test (#4)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
+  it { is_expected.to validate_presence_of(:date_of_birth) }
+  it { is_expected.to allow_value('valid@email.com').for(:email) }
+  it { is_expected.not_to allow_value('invalid_email').for(:email) }
+  it { is_expected.to validate_confirmation_of(:password) }
+
+  context 'when creating a user with a role' do
+    it 'creates a user with the specified role' do
+      user = create(:user, role_name: 'teacher')
+      expect(user.roles.first.name).to eq('teacher')
+    end
+
+    it 'creates a student by default' do
+      user = create(:user)
+      expect(user.roles.first.name).to eq('student')
+    end
+
+    it 'returns users with the specified role' do
+      teacher = create(:user, role_name: 'teacher')
+      create(:user, role_name: 'student')
+      expect(described_class.by_role('teacher')).to eq([teacher])
+    end
+  end
 end
