3c465f6e6dd9ef7a70de132398d2283b4610fa7b
stanislav <stas.plisko.99@mail.ru>
2024-04-13 21:02:15 +0300

    validates for student

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,9 @@
 class Student < ApplicationRecord
-  belongs_to :user
-  belongs_to :speciality
-  belongs_to :group
+  belongs_to :user, required: true
+  belongs_to :speciality, required: true
+  belongs_to :group, required: true
+
+  validates :user, presence: true
+  validates :speciality, presence: true
+  validates :group, presence: true
 end
