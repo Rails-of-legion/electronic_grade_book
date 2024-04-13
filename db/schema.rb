08d0bbc1df6370ce4dfe4910d277c4b573409ef7
stanislav <stas.plisko.99@mail.ru>
2024-04-13 20:50:53 +0300

    add specialities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_13_154139) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_13_174440) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_13_154139) do
     t.datetime "updated_at", null: false
     t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
     t.index ["resource_type", "resource_id"], name: "index_roles_on_resource"
+  end
+
+  create_table "specializations", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
---
+++ spec/factories/specializations.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :specialization do
+    name { "MyString" }
+  end
+end
