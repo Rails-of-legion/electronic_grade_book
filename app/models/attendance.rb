ad847faf4c17fa3fbb08f1773eb37129cfed1850
MatveySviadysh <mkomp06@gmail.com>
2024-04-14 12:17:20 +0300

    wrote validates for model attendance

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,2 +1,5 @@
 class Attendance < ApplicationRecord
+  validates :date, presence: true
+  validates :attendance_status, presence: true
+  
 end
--- db/migrate/20240414091232_create_attendances.rb
+++ db/migrate/20240414091232_create_attendances.rb
@@ -1,10 +1,10 @@
 class CreateAttendances < ActiveRecord::Migration[7.1]
   def change
     create_table :attendances do |t|
-      t.integer :subject_id
-      t.integer :student_id
-      t.date :date
-      t.string :attendance_status
+      t.integer :subject_id, null: false, foreign_key: true
+      t.integer :student_id, null: false, foreign_key: true
+      t.date :date, null: false
+      t.string :attendance_status, null: false
 
       t.timestamps
     end
