b16d2e2c5eab225e45ceacc2e5340292da67d905
stanislav <stas.plisko.99@mail.ru>
2024-04-14 14:43:50 +0300

    refactoring model  and test

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,32 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
+  # Ассоциации
   it { should belong_to(:user).required }
   it { should belong_to(:specialization).required }
   it { should belong_to(:group).required }
-  it { should validate_presence_of(:user) }
-  it { should validate_presence_of(:specialization) }
-  it { should validate_presence_of(:group) }
 
-  it "creates a student with valid attributes" do
-    user = FactoryBot.create(:user)
-    specialization = FactoryBot.create(:specialization)
-    group = FactoryBot.create(:group)
-
-    student = FactoryBot.build(:student, user: user, specialization: specialization, group: group)
-    expect(student.save).to be_truthy
-  end
-
-  it "does not create a student without associated objects" do
-    student = FactoryBot.build(:student, user: nil, specialization: nil, group: nil)
-    expect(student.save).to be_falsey
-    expect(student.errors.full_messages).to include(
-      "User must exist",
-      "Specialization must exist",
-      "Group must exist",
-      "User can't be blank",
-      "Specialization can't be blank",
-      "Group can't be blank"
-    )
+  # Тест наличия связанных объектов
+  describe 'associations' do
+    it 'is invalid without a user' do
+      student = build(:student, user: nil)
+      expect(student).to_not be_valid
+      expect(student.errors[:user]).to include("must exist")
     end
+
+    it 'is invalid without a specialization' do
+      student = build(:student, specialization: nil)
+      expect(student).to_not be_valid
+      expect(student.errors[:specialization]).to include("must exist")
+    end
+
+    it 'is invalid without a group' do
+      student = build(:student, group: nil)
+      expect(student).to_not be_valid
+      expect(student.errors[:group]).to include("must exist")
+    end
+  end
 end
