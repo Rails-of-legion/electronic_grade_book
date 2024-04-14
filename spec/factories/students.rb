8c9a973976a005bb341d1adf7875a3e2478ca868
stanislav <stas.plisko.99@mail.ru>
2024-04-14 15:15:55 +0300

    refactoring

--- spec/factories/students.rb
+++ spec/factories/students.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :student do
-    association :user, strategy: :build 
+    association :user, strategy: :build
     association :specialization, strategy: :build
     association :group, strategy: :build
   end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,29 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe Student, type: :model do
+RSpec.describe Student do
   # Ассоциации
-  it { should belong_to(:user).required }
-  it { should belong_to(:specialization).required }
-  it { should belong_to(:group).required }
+  it { is_expected.to belong_to(:user).required }
+  it { is_expected.to belong_to(:specialization).required }
+  it { is_expected.to belong_to(:group).required }
 
   # Тест наличия связанных объектов
   describe 'associations' do
     it 'is invalid without a user' do
       student = build(:student, user: nil)
-      expect(student).to_not be_valid
-      expect(student.errors[:user]).to include("must exist")
+      expect(student).not_to be_valid
+      expect(student.errors[:user]).to include('must exist')
     end
 
     it 'is invalid without a specialization' do
       student = build(:student, specialization: nil)
-      expect(student).to_not be_valid
-      expect(student.errors[:specialization]).to include("must exist")
+      expect(student).not_to be_valid
+      expect(student.errors[:specialization]).to include('must exist')
     end
 
     it 'is invalid without a group' do
       student = build(:student, group: nil)
-      expect(student).to_not be_valid
-      expect(student.errors[:group]).to include("must exist")
+      expect(student).not_to be_valid
+      expect(student.errors[:group]).to include('must exist')
     end
   end
 end
