dc8c7cee272ff3489bd4f6adae0c5b829e9da02c
stanislav <stas.plisko.99@mail.ru>
2024-04-16 12:47:26 +0300

    fix test and model for user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :user do
-    name { Faker::Name.name }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    middle_name { Faker::Name.middle_name }
+    phone_number { Faker::PhoneNumber.phone_number }
     date_of_birth { Faker::Date.birthday(min_age: 18, max_age: 65) }
     status { true }
     email { Faker::Internet.unique.email }
