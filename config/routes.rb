8b9953e54bdfdb35adb8206c4e6ca699b15cc428
Sergick12 <cepgruddd1@gmail.com>
2024-04-16 19:37:08 +0300

    added semesters

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home#home"
   resources :users, only: %i[index show] 
+  resources :semesters
   get 'about', to: 'home#about', as: :about
 end
 
