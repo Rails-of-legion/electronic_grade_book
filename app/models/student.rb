2f8c490233337fa5da5ec04f2855ffe467e559c6
stanislav <stas.plisko.99@mail.ru>
2024-04-16 10:52:23 +0300

    added retakes

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,5 @@ class Student < ApplicationRecord
   belongs_to :specialization, optional: false
   belongs_to :group, optional: false
   has_one :record_book, dependent: :destroy
+  has_many :retakes, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_15_134231) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_16_061412) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,6 +62,18 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_15_134231) do
     t.index ["student_id"], name: "index_record_books_on_student_id"
     t.index ["subject_id"], name: "index_record_books_on_subject_id"
     t.index ["teacher_id"], name: "index_record_books_on_teacher_id"
+  end
+
+  create_table "retakes", force: :cascade do |t|
+    t.bigint "subject_id", null: false
+    t.bigint "student_id", null: false
+    t.date "date"
+    t.bigint "grade_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["grade_id"], name: "index_retakes_on_grade_id"
+    t.index ["student_id"], name: "index_retakes_on_student_id"
+    t.index ["subject_id"], name: "index_retakes_on_subject_id"
   end
 
   create_table "roles", force: :cascade do |t|
@@ -148,6 +160,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_15_134231) do
   add_foreign_key "record_books", "students"
   add_foreign_key "record_books", "subjects"
   add_foreign_key "record_books", "users", column: "teacher_id"
+  add_foreign_key "retakes", "grades"
+  add_foreign_key "retakes", "students"
+  add_foreign_key "retakes", "subjects"
   add_foreign_key "students", "groups"
   add_foreign_key "students", "specializations"
   add_foreign_key "students", "users"
