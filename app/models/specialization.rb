41dedc917f0ab3b7429dcf1dad09fa235d3da94c
Baron-Dmitry <88362667+Baron-Dmitry@users.noreply.github.com>
2024-04-18 16:34:13 +0300

    [EGB-202] add notifications, specializations, retake in admin panel

--- app/models/specialization.rb
+++ app/models/specialization.rb
@@ -2,4 +2,11 @@ class Specialization < ApplicationRecord
   has_many :students, dependent: :destroy
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
+
+  def self.ransackable_associations(auth_object = nil)
+    ["students"]
+  end
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "id_value", "name", "updated_at"]
+  end
 end
