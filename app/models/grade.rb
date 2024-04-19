7b0265bec670bdcd7e9248e412fbced8a4ee43e1
stanislav <stas.plisko.99@mail.ru>
2024-04-19 10:44:22 +0300

    refactoring

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -1,8 +1,4 @@
 class Grade < ApplicationRecord
   belongs_to :record_book
   validates :grade, presence: true
-
-  has_one :subject, through: :record_book
-  has_one :semester, through: :subject
-  has_one :teacher, through: :record_book
 end
