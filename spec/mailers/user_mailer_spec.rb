04e70ccc14025fa74f8df1abb4e4bcf327e957d4
Sergick12 <cepgruddd1@gmail.com>
2024-04-20 15:43:52 +0300

    run rubocop

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,18 +1,17 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe UserMailer, type: :mailer do
-  describe "registration_confirmation" do
-    let(:mail) { UserMailer.registration_confirmation }
+RSpec.describe UserMailer do
+  describe 'registration_confirmation' do
+    let(:mail) { described_class.registration_confirmation }
 
-    it "renders the headers" do
-      expect(mail.subject).to eq("Registration confirmation")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+    it 'renders the headers' do
+      expect(mail.subject).to eq('Registration confirmation')
+      expect(mail.to).to eq(['to@example.org'])
+      expect(mail.from).to eq(['from@example.com'])
     end
 
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+    it 'renders the body' do
+      expect(mail.body.encoded).to match('Hi')
     end
   end
-
 end
