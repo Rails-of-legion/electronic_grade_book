257db7aa9a7e34f6d9b526b156dd079f31d7e196
stanislav <stas.plisko.99@mail.ru>
2024-04-22 17:38:36 +0300

    refactoring

--- app/admin/grades.rb
+++ app/admin/grades.rb
@@ -1,24 +1,24 @@
 ActiveAdmin.register Grade do
-    permit_params :record_book_id, :grade
-  
-    index do
-      selectable_column
-      id_column
-      column :record_book
-      column :grade
-      column :created_at
-      actions
+  permit_params :record_book_id, :grade
+
+  index do
+    selectable_column
+    id_column
+    column :record_book
+    column :grade
+    column :created_at
+    actions
+  end
+
+  filter :record_book
+  filter :grade
+  filter :created_at
+
+  form do |f|
+    f.inputs do
+      f.input :record_book
+      f.input :grade
     end
-  
-    filter :record_book
-    filter :grade
-    filter :created_at
-  
-    form do |f|
-      f.inputs do
-        f.input :record_book
-        f.input :grade
-      end
-      f.actions
-    end
+    f.actions
+  end
 end
--- app/admin/students.rb
+++ app/admin/students.rb
@@ -1,27 +1,27 @@
 ActiveAdmin.register Student do
-    permit_params :user_id, :specialization_id, :group_id
-  
-    index do
-      selectable_column
-      id_column
-      column :user
-      column :specialization
-      column :group
-      column :created_at
-      actions
+  permit_params :user_id, :specialization_id, :group_id
+
+  index do
+    selectable_column
+    id_column
+    column :user
+    column :specialization
+    column :group
+    column :created_at
+    actions
+  end
+
+  filter :user
+  filter :specialization
+  filter :group
+  filter :created_at
+
+  form do |f|
+    f.inputs do
+      f.input :user
+      f.input :specialization
+      f.input :group
     end
-  
-    filter :user
-    filter :specialization
-    filter :group
-    filter :created_at
-  
-    form do |f|
-      f.inputs do
-        f.input :user
-        f.input :specialization
-        f.input :group
-      end
-      f.actions
-    end
+    f.actions
+  end
 end
