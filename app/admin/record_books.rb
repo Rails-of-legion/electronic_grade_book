89aa5cf69bad5bfc9392d9e61573d51f0016de27
Baron-Dmitry <emptyinside08@gmail.com>
2024-04-22 21:15:40 +0300

    another fix

--- app/admin/record_books.rb
+++ app/admin/record_books.rb
@@ -1,18 +1,19 @@
 ActiveAdmin.register RecordBook do
-  permit_params :subject_id, :student_id, :teacher_id, :intermediate_attestation_id
+  permit_params :student_id, :teacher_id, :intermediate_attestation_id
 
   index do
     selectable_column
     id_column
-    column :subject
     column :student
     column :teacher
     column :intermediate_attestation
+    column 'Subjects' do |record_book|
+      record_book.subjects.map(&:name).join(', ').html_safe
+    end
     column :created_at
     actions
   end
 
-  filter :subject
   filter :student
   filter :teacher
   filter :intermediate_attestation
@@ -20,10 +21,10 @@ ActiveAdmin.register RecordBook do
 
   form do |f|
     f.inputs do
-      f.input :subject
       f.input :student
       f.input :teacher
       f.input :intermediate_attestation
+      f.input :subjects, as: :check_boxes, collection: Subject.all.map { |subject| [subject.name, subject.id] }
     end
     f.actions
   end
