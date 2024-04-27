55683322c5c7275bc06a34757b0ccc5c82a5cb32
stanislav <stas.plisko.99@mail.ru>
2024-04-27 19:54:09 +0300

    added seeds

--- app/models/specialization.rb
+++ app/models/specialization.rb
@@ -2,6 +2,7 @@ class Specialization < ApplicationRecord
   has_many :specialities_subjects, class_name: 'SpecialitiesSubject', dependent: :destroy
   has_many :subjects, through: :specialities_subjects
   has_many :record_books, dependent: :destroy
+  has_many :groups, dependent: :destroy
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 
