53dfb7c4e749375ed6bc3e2cfd77a6d4060f7124
Sergick12 <cepgruddd1@gmail.com>
2024-05-03 13:46:52 +0300

    added seeds for notifications and change db

--- db/migrate/20240415160918_create_notifications.rb
+++ db/migrate/20240415160918_create_notifications.rb
@@ -3,7 +3,6 @@ class CreateNotifications < ActiveRecord::Migration[7.1]
     create_table :notifications do |t|
       t.text :message
       t.datetime :date
-      t.boolean :status
 
       t.timestamps
     end
