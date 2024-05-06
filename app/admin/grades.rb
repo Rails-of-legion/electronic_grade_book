fcc7f3e5d2dbaac2db0e77b4a83f53a48f97f119
stanislav <stas.plisko.99@mail.ru>
2024-05-06 21:55:06 +0300

    refactoring grades for admin panel

--- app/admin/grades.rb
+++ app/admin/grades.rb
@@ -1,20 +1,21 @@
 ActiveAdmin.register Grade do
-  permit_params :record_book_id, :subject_id, :grade, :date
+  permit_params :grade, :date, :subject_id, :record_book_id
 
   index do
     selectable_column
     id_column
+    column :student do |grade|
+      grade.record_book.user.name
+    end
     column :record_book
+    column :subject
     column :grade
     column :date
-    column :subject do |grade|
-      grade.record_book.specialization.subjects
-    end
     column :created_at
     actions
   end
 
-  filter :record_book
+  filter :record_book_user_first_name, as: :string, filters: [:cont], label: 'Student Name'
   filter :subject
   filter :grade
   filter :date
@@ -22,13 +23,19 @@ ActiveAdmin.register Grade do
 
   form do |f|
     f.inputs do
-      f.input :record_book
-      f.input :subject do |grade|
-        grade.record_book.specialization.subjects
-      end
+      f.input :record_book, as: :select, collection: RecordBook.joins(:user).where(users: { id: User.with_role(:student).pluck(:id) }), label: 'Студент', member_label: Proc.new { |rb| rb.user.name }
+      f.input :subject
       f.input :grade
       f.input :date
     end
     f.actions
+  end
+  
+
+  controller do
+    def create
+      @grade = Grade.new(permitted_params[:grade])
+      create!
+    end
   end
 end
