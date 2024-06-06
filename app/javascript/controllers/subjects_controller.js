6552c7948812294973d44932ba288ec739120987
Sergick12 <cepgruddd1@gmail.com>
2024-06-06 15:26:33 +0300

    fix importmap

--- app/javascript/controllers/subjects_controller.js
+++ app/javascript/controllers/subjects_controller.js
@@ -1,12 +1,39 @@
+// app/javascript/controllers/subjects_controller.js
 import { Controller } from "@hotwired/stimulus"
-import {get} from "@rails/request.js"
 
 export default class extends Controller {
-  connect()
-  {
-    console.log("connected")
+  static targets = [
+    "specialization", 
+    "group", 
+    "recordBook", 
+    "intermediateAttestation"
+  ]
+  initialize() {
+    console.log('con')
   }
-  change(event){
-    console.log(event.target.selectedOptions[0].value)
+  change(event) {
+    const target = event.target;
+    const nextTarget = this.getNextTarget(target);
+
+    if (nextTarget) {
+      this.enableSelect(nextTarget);
+    }
+  }
+
+  getNextTarget(current) {
+    switch (current) {
+      case this.specializationTarget:
+        return this.groupTarget;
+      case this.groupTarget:
+        return this.recordBookTarget;
+      case this.recordBookTarget:
+        return this.intermediateAttestationTarget;
+      default:
+        return null;
+    }
+  }
+
+  enableSelect(select) {
+    select.disabled = false;
   }
 }
