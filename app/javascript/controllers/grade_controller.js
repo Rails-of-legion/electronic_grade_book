1d9f26a896edd31a73ea3efa16e94bc5c761603a
baron-dmitry <emptyinside08@gmail.com>
2024-06-06 22:12:33 +0300

    js

--- app/javascript/controllers/grade_controller.js
+++ app/javascript/controllers/grade_controller.js
@@ -27,20 +27,54 @@ export default class extends Controller {
   }
 
   change(event) {
-    const subjectSelect = this.subjectSelectTarget
+    console.log(event.target.selectedOptions[0].value)
+    this.updateFieldsState()
+  }
+
+  changeSubject(event) {
+    console.log(event.target.selectedOptions[0].value)
     const dateSelect = this.hasDateSelectTarget ? this.dateSelectTarget : null
     const gradeSelect = this.hasGradeSelectTarget ? this.gradeSelectTarget : null
     const recordBookSelect = this.hasRecordBookSelectTarget ? this.recordBookSelectTarget : null
     const formTeacher = this.hasFormTeacherTarget ? this.formTeacherTarget : null
-
-    if (event.target.value) {
-      subjectSelect.disabled = false
+  
+    // Получаем значения month и subject_id
+    const month = document.getElementById('month-select').value
+    const subjectId = event.target.value
+  
+    // Обновляем атрибуты элемента formTeacher
+    formTeacher.setAttribute('data-month', month)
+    formTeacher.setAttribute('data-subject-id', subjectId)
+  
+    if (subjectId) {
+      if (dateSelect) dateSelect.disabled = false
+      if (gradeSelect) gradeSelect.disabled = false
+      if (recordBookSelect) recordBookSelect.disabled = false
+      if (formTeacher) formTeacher.style.display = 'block'
     } else {
-      subjectSelect.disabled = true
       if (dateSelect) dateSelect.disabled = true
       if (gradeSelect) gradeSelect.disabled = true
       if (recordBookSelect) recordBookSelect.disabled = true
       if (formTeacher) formTeacher.style.display = 'none'
+    }
+    
+    this.reloadFormTeacher();
+  }
+
+  reloadFormTeacher() {
+    const monthValue = document.getElementById('month-select').value;
+    const subjectValue = this.subjectSelectTarget.value;
+    
+    if (monthValue && subjectValue) {
+      const formTeacherContainer = this.formTeacherTarget;
+      const url = `/grades/update_form_teacher?month=${monthValue}&subject_id=${subjectValue}`;
+
+      fetch(url)
+        .then(response => response.text())
+        .then(html => {
+          formTeacherContainer.innerHTML = html;
+        })
+        .catch(error => console.error('Error reloading form teacher:', error));
     }
   }
 
