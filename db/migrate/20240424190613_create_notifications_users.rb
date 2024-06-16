4b180a80b19c86d82a3724c1d2aa97edb92bdab6
Сергей Коноплицкий <pl1tz2021@gmail.com>
2024-06-16 21:50:56 +0300

    fix [EGB-449] fix nitifications for personal profile (#121)

--- db/migrate/20240424190613_create_notifications_users.rb
+++ db/migrate/20240424190613_create_notifications_users.rb
@@ -3,7 +3,7 @@ class CreateNotificationsUsers < ActiveRecord::Migration[7.1]
     create_table :notifications_users do |t|
       t.references :notification, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
-      t.boolean :status
+      t.boolean :status, default: false 
       
       t.timestamps
     end
