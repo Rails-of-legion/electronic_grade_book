3cc4e7978e11af79e9462226d0a4d8045d0dcd83
pl1tz <pl1tz2021@gmail.com>
2024-06-24 19:18:33 +0300

    fix [EGB-466] translate project

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,20 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_21_173655) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string "namespace"
+    t.text "body"
+    t.string "resource_type"
+    t.bigint "resource_id"
+    t.string "author_type"
+    t.bigint "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author"
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource"
+  end
+
   create_table "grades", force: :cascade do |t|
     t.bigint "subject_id", null: false
     t.integer "grade"
