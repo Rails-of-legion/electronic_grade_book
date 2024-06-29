6defd9c018fa62db88e9f729e1f4db5de4805b9e
Kirill Byckov <kirill.bykov.2000@mail.ru>
2024-06-29 16:32:47 +0300

    delete button

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,28 +4,5 @@ import "./controllers";
 import * as bootstrap from "bootstrap"
 import 'bootstrap/dist/js/bootstrap'
 import "bootstrap/dist/css/bootstrap";
-import 'bootstrap-icons/font/bootstrap-icons.css';
 
-import { Button } from 'bootstrap';
-import 'bootstrap/dist/js/bootstrap.bundle';
-
-function scrollToTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-}
-
-document.addEventListener("turbo:load", function() {
-    var scrollToTopButton = document.getElementById('scrollToTopButton');
-
-    window.addEventListener("scroll", function() {
-        if (window.scrollY > 300) {
-            scrollToTopButton.classList.remove('d-none');
-        } else {
-            scrollToTopButton.classList.add('d-none');
-        }
-    });
-
-    scrollToTopButton.addEventListener("click", function() {
-        scrollToTop();
-    });
-});
 
