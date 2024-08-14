188b11a177b0e8894d46ac3560c2e9eb96090aeb
Mardziakhin Stanislau <mardziakhin.stanislau@gmail.com>
2024-08-14 18:52:52 +0300

    feat EGB-058 test specialization, subject

--- spec/factories/subjects.rb
+++ spec/factories/subjects.rb
@@ -2,7 +2,5 @@ FactoryBot.define do
   factory :subject do
     name { Faker::Educator.subject }
     description { Faker::Lorem.paragraph }
-    assessment_type { Faker::Educator.course_name }
-    association :semester, strategy: :build
   end
 end
