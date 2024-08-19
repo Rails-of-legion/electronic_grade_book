9c6bfeb6b0f29ab41ddff5512e90ad334807b2d6
pl1tz <pl1tz2021@gmail.com>
2024-08-19 16:36:51 +0300

    feat [EGB-063] rspec groups controller

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :group do
-    name { Faker::Lorem.word }
-    curator factory: %i[user]
-    specialization { create(:specialization) }
-    form_of_education {'Очная'}
+    name { "Group #{Faker::Number.unique.number(digits: 2)}" }
+    form_of_education { %w[full_time part_time].sample }
+    association :curator, factory: :user
+    association :specialization
   end
 end
