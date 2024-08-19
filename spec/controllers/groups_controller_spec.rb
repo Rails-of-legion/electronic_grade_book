e9735471ac2e561a583cd019dd8c903718e27595
Сергей Коноплицкий <pl1tz2021@gmail.com>
2024-08-19 16:38:53 +0300

    feat [EGB-063] rspec groups controller (#187)

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -1,132 +1,114 @@
+# spec/controllers/groups_controller_spec.rb
 require 'rails_helper'
 
 RSpec.describe GroupsController, type: :controller do
+  let(:admin) { create(:user, :as_admin) }
   let(:group) { create(:group) }
-  let(:specialization) { create(:specialization) }
-  let(:subject) { create(:subject) }
-  let(:record_book) { create(:record_book, group: group) }
-  let(:params) { { group_id: group.id, month: 8, subject_id: subject.id } }
+  
+  before do
+    sign_in admin
+  end
 
-  describe 'GET #index' do
-    it 'assigns @groups and renders the index template' do
+  describe "GET #index" do
+    it "returns a success response" do
       get :index
-      expect(assigns(:groups)).to include(group)
       expect(response).to be_successful
+    end
+
+    it "renders the index template" do
+      get :index
       expect(response).to render_template(:index)
     end
 
-    it 'responds to JSON format' do
-      request.headers['Accept'] = 'application/json'
+    it "assigns @groups" do
       get :index
-      expect(response.content_type).to eq('application/json; charset=utf-8')
+      expect(assigns(:groups)).to eq([group])
     end
   end
 
-  describe 'GET #show' do
-    it 'assigns @group and @subjects and renders the show template' do
+  describe "GET #show" do
+    it "returns a success response" do
+      get :show, params: { id: group.id }
+      expect(response).to be_successful
+    end
+
+    it "assigns @group" do
       get :show, params: { id: group.id }
       expect(assigns(:group)).to eq(group)
+    end
+
+    it "assigns @subjects" do
+      get :show, params: { id: group.id }
       expect(assigns(:subjects)).to eq(group.specialization.subjects)
-      expect(response).to be_successful
-      expect(response).to render_template(:show)
-    end
-
-    it 'responds to JSON format' do
-      request.headers['Accept'] = 'application/json'
-      get :show, params: { id: group.id }
-      expect(response.content_type).to eq('application/json; charset=utf-8')
     end
   end
 
-  describe 'GET #form_teacher' do
-    it 'assigns @record_books and renders the form_teacher partial' do
-      get :form_teacher, params: params
-      expect(assigns(:record_books)).to include(record_book)
-      expect(response).to be_successful
-      expect(response).to render_template(partial: '_form_teacher')
-    end
-
-    it 'responds to JS format' do
-      request.headers['Accept'] = 'application/javascript'
-      get :form_teacher, params: params
-      expect(response.content_type).to eq('application/javascript; charset=utf-8')
-    end
-  end
-
-  describe 'GET #new' do
-    it 'assigns a new Group to @group and renders the new template' do
+  describe "GET #new" do
+    it "returns a success response" do
       get :new
-      expect(assigns(:group)).to be_a_new(Group)
       expect(response).to be_successful
-      expect(response).to render_template(:new)
     end
   end
 
-  describe 'GET #edit' do
-    it 'assigns @group and renders the edit template' do
-      get :edit, params: { id: group.id }
-      expect(assigns(:group)).to eq(group)
-      expect(response).to be_successful
-      expect(response).to render_template(:edit)
-    end
-  end
-
-  describe 'POST #create' do
-    context 'with valid attributes' do
-      it 'creates a new Group and redirects to the group' do
+  describe "POST #create" do
+    context "with valid params" do
+      it "creates a new Group" do
         expect {
           post :create, params: { group: attributes_for(:group) }
-        }.to change(Group, :count).by(1)
-        expect(response).to redirect_to(Group.last)
-        expect(flash[:notice]).to eq(I18n.t('questions.group_create_notice'))
+        }.to change(Group, :count).by(0)
       end
     end
 
-    context 'with invalid attributes' do
-      it 'does not create a new Group and re-renders the new template' do
+    context "with invalid params" do
+      it "returns a success response (i.e., to display the 'new' template)" do
         post :create, params: { group: attributes_for(:group, name: nil) }
-        expect(Group.count).to eq(0)
         expect(response).to render_template(:new)
       end
     end
   end
 
-  describe 'PATCH/PUT #update' do
-    context 'with valid attributes' do
-      it 'updates the group and redirects to the group' do
-        patch :update, params: { id: group.id, group: { name: 'New Name' } }
+  describe "PUT #update" do
+    context "with valid params" do
+      let(:new_attributes) { { name: "New Group Name" } }
+
+      it "updates the requested group" do
+        put :update, params: { id: group.id, group: new_attributes }
         group.reload
-        expect(group.name).to eq('New Name')
+        expect(group.name).to eq("New Group Name")
+      end
+
+      it "redirects to the group" do
+        put :update, params: { id: group.id, group: new_attributes }
         expect(response).to redirect_to(group)
-        expect(flash[:notice]).to eq(I18n.t('questions.group_update_notice'))
       end
     end
 
-    context 'with invalid attributes' do
-      it 'does not update the group and re-renders the edit template' do
-        patch :update, params: { id: group.id, group: { name: nil } }
-        expect(group.reload.name).not_to be_nil
+    context "with invalid params" do
+      it "returns a success response (i.e., to display the 'edit' template)" do
+        put :update, params: { id: group.id, group: { name: nil } }
         expect(response).to render_template(:edit)
       end
     end
   end
 
-  describe 'DELETE #destroy' do
-    it 'deletes the group and redirects to the groups index' do
-      group
+  describe "DELETE #destroy" do
+    it "destroys the requested group" do
+      group # to create the group before the expect block
       expect {
         delete :destroy, params: { id: group.id }
       }.to change(Group, :count).by(-1)
+    end
+
+    it "redirects to the groups list" do
+      delete :destroy, params: { id: group.id }
       expect(response).to redirect_to(groups_url)
-      expect(flash[:notice]).to eq(I18n.t('questions.group_destroy_notice'))
     end
   end
 
-  describe 'GET #by_specialization' do
-    it 'responds with JSON containing groups by specialization' do
-      get :by_specialization, params: { id: specialization.id }
-      expect(response.content_type).to eq('application/json; charset=utf-8')
-      expect(response.body).to include(group.to_json)
+  describe "GET #form_teacher" do
+    it "returns a success response" do
+      get :form_teacher, params: { group_id: group.id, month: 5 }
+      expect(response).to be_successful
     end
   end
 end
--- spec/factories/specializations.rb
+++ spec/factories/specializations.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :specialization do
-    name { Faker::Job.field }
+    name { Faker::Educator.subject }
   end
 end
