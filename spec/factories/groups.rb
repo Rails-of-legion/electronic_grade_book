143d503fe8b7abad0c06440517f8dd572bcd4c90
pl1tz <pl1tz2021@gmail.com>
2024-08-21 18:55:52 +0300

    feat [EGB-065] rspec individual reports contpoller

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :group do
-    name { "Group #{SecureRandom.hex(3)}" }  # Генерация уникального имени группы
+    name { "Unique Group #{SecureRandom.hex(5)}" } # Генерация уникального имени группы
     form_of_education { %w[full_time part_time].sample }
     association :curator, factory: :user
     association :specialization
