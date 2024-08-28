73b50520283a1963a22781d88305944b1d2feab2
Kirill <kirill.bck.20000@mail.ru>
2024-08-28 03:58:33 +0300

    add new fields to subject

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_21_173655) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_28_002036) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -137,6 +137,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_21_173655) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "hours"
+    t.integer "credit_units"
   end
 
   create_table "teachers_subjects", force: :cascade do |t|
