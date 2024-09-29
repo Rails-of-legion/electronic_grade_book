b79098d6a2e1b2cf0a6583030a5a609773c49044
sedaksiii <sedaksizhurko@gmail.com>
2024-09-29 22:16:53 +0200

    On branch fix/EGB-500/edit_disciplines  Changes to be committed: 	new file:   Gemfile copy 	modified:   app/controllers/subjects_controller.rb 	modified:   app/models/subject.rb 	modified:   app/views/subjects/_form.html.erb 	modified:   app/views/subjects/show.html.erb 	modified:   config/locales/en.yml 	modified:   config/locales/ru.yml 	new file:   db/migrate/20240927123221_add_profiling_to_subjects.rb 	modified:   db/schema.rb

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -4,6 +4,7 @@ class Subject < ApplicationRecord
   validates :hours, presence: true
   validates :credit_units, presence: true
 
+
   has_many :semesters_subjects, dependent: :destroy
   has_many :semesters, through: :semesters_subjects, dependent: :destroy
   has_many :grades, dependent: :destroy
