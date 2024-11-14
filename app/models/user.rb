a0cdbe030654c24d8a9f8bc545d13fba16d9b920
pl1tz <pl1tz2021@gmail.com>
2024-11-14 18:01:17 +0300

    add colum group in table users

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,7 @@ class User < ApplicationRecord
   has_many :subjects, through: :teachers_subjects
   has_one :record_book, dependent: :destroy
   has_many :intermediate_attestation, foreign_key: :teacher_id, dependent: :destroy
+  
 
   before_save :set_expelled_at
   after_update :schedule_deletion_if_expelled
@@ -27,6 +28,7 @@ class User < ApplicationRecord
     super + %w[record_book roles]
   end
 
+  
   def self.ransackable_attributes(_auth_object = nil)
     %w[
       created_at
@@ -65,6 +67,7 @@ class User < ApplicationRecord
   def format_full_name
     "#{last_name} #{first_name[0]}.#{middle_name[0]}."
   end
+
 
   private
 
